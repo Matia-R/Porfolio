7b1b0d09b420635e7e443a1bf9b1eab6a02af183
Matia Raspopovic <matia@raspopovic.ca>
2021-12-24 15:32:32 -0500

    Setup Navbar UI component

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
