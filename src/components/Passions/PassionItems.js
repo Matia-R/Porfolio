fc1f59940e14d0537f76438422f35746bd4a4396
Matia Raspopovic <matia@raspopovic.ca>
2022-01-04 20:20:17 -0500

    improved project + passion copywrite

--- src/components/Passions/PassionItems.js
+++ src/components/Passions/PassionItems.js
@@ -2,7 +2,7 @@ let passionItems = [
     {
         title: "Rugby",
         imgUrl: process.env.PUBLIC_URL + '/img/rugby_img.png',
-        description: "In my first year of High School, I decided to try Rugby on whim. Nearly a decade since then, I’m blown away by how big an influence this sport has been in my life. I’ve learned many important lessons about patience, determination, and camaraderie on the pitch. During my time playing in High School, I worked hard to learn the sport, refine my skills, and eventually contribute to a City Championship victory in my final year. I currently play for the Carleton University Men’s Rugby Team where we’ve been successful in obtaining a championship victory in 2019 and a successful playoff campaign in the 2021 season.",
+        description: "In my first year of High School, I decided to try Rugby on whim. Nearly a decade since then, I’m blown away by how how much this sport means to me - I’ve learned so many important lessons about patience, determination, and camaraderie on the pitch. During my time playing in High School, I worked hard to learn the sport, refine my skills, and eventually contribute to a City Championship victory in my final year. I currently play for the Carleton University Men’s Rugby Team where we've achieved a championship victory in 2019 and a successful playoff campaign in the 2021 season.",
         tags: [],
         linkUrl: "https://goravens.ca/clubs/mens-rugby/",
         linkText: "Learn More"
@@ -10,7 +10,7 @@ let passionItems = [
     {
         title: "Mentoring",
         imgUrl: process.env.PUBLIC_URL + '/img/sssc_img.png',
-        description: "I’m a big believer that nobody is “self-made”. Whether it be for sports, academics, or my career, I have received more than my fair share of help from experienced mentors. For this reason, I decided to join the Science Student Success Center at Carleton University, where I’ve had the pleasure of mentoring Computer Science students for over a year now. In this role I’ve worked closely with students to help them define their goals and make plans to achieve them. I’ve also had the opportunity of hosting several workshops on industry technologies such as git. Throughout my mentorship I’ve been fortunate to see many of my students succeed which gives me a great sense of pride.",
+        description: "I’m a big believer that nobody is “self-made”. Whether it be for sports, academics, or my career, I have received more than my fair share of help. For this reason, I joined the Science Student Success Center at Carleton University, where I’ve had the pleasure of mentoring Computer Science students for over a year now. In this role I’ve worked closely with students to help them define their goals and make plans to achieve them. I’ve also had the opportunity to host several workshops on industry technologies such as git. I’ve been fortunate to see many of my students succeed which gives me a great sense of pride.",
         tags: [],
         linkUrl: "https://sssc.carleton.ca/matia-r",
         linkText: "Learn More"
@@ -24,7 +24,7 @@ let passionItems = [
     {
         title: "Skiing",
         imgUrl: process.env.PUBLIC_URL + '/img/ski_img.png',
-        description: "I’m part of a family of full-blown ski-addicts. My dad had me in skis as soon as I took my first steps, so I have countless amazing memories on the slopes. While I mainly ski as a hobby I did compete on my High School Varisty Ski team and won the Most Improved Athlete award in 2015 which was a rewarding experience. I currently have a family trip to B.C. planned where we’ll be skiing at Revelstoke and Kicking Horse (and I'm beyon excited). If you have any cool skiing experiences you want to share, please reach out, I’d love to hear all about it!",
+        description: "I’m part of a family of full-blown ski-addicts. My dad had me in skis as soon as I took my first steps, so I have countless amazing memories on the slopes. While I mainly ski as a hobby I did compete on my High School Varisty Ski team and won the Most Improved Athlete award in 2015 which was super rewarding. Currently, I'm beyond excited for a trip I have planned to ski at the legendary Revelstoke and Kicking Horse mountians in BC this winter. If you have any cool skiing experiences you want to share, please reach out, I’d love to hear all about it!",
         tags: []
     }
 ]
--- src/components/Projects/ProjectItems.js
+++ src/components/Projects/ProjectItems.js
@@ -2,7 +2,7 @@ let projectItems = [
     {
         title: "Digital Portfolio",
         imgUrl: process.env.PUBLIC_URL + '/img/portfolio_img.png',
-        description: "A blurb about my Portfolio Website, on my Portfolio website. A Computer Science student might call that a bit ‘recursive’. All jokes aside, this project was a lot of fun to make. My biggest takeaway from this experience was getting to learn React and leverage hooks and states for a more dynamic UI. I also found React components to be incredibly useful for UI templating which allowed me to focus on improving my styling skills. I plan to add to this site periodically and take what I’ve learned to help me build web applications with more substantial backends. Please take a look at my code if you’re interested and feel free to leave feedback. I’m always eager to improve my projects and learn from others!",
+        description: "A blurb about my Portfolio Website, on my Portfolio website. A Computer Science student might call that a bit ‘recursive’. All jokes aside, this project was challenging but a lot of fun to make. My biggest takeaway from this experience was getting to learn React and leverage hooks and states for a more dynamic UI. I also found React to be incredibly useful for UI component reusability which allowed me to focus on improving my styling skills. I plan to add to this site periodicacally and use it as a hub to showcase my future experiences and projects. Please take a look at my code if you’re interested and feel free to leave feedback. I’m always eager to improve my projects and learn from others!",
         tags: [
             "JavaScript",
             "Node.js",
@@ -18,7 +18,7 @@ let projectItems = [
     {
         title: "Advantage Academy",
         imgUrl: process.env.PUBLIC_URL + '/img/advantage_academy_img.png',
-        description: "Advantage Academy is an online tutoring business I co-founded with the goal of providing safe and affordable assistance to high school students during the Covid-19 pandemic. Since our launch in 2020, our client base has quadrupled, and we’ve been fortunate enough to welcome a second and third wave of tutors to our team. In addition to managing the tutoring team of tutors, I’ve leveraged my technical background to build and maintain the Company website as well as devise automated solutions to our billing and payroll process. Going forward we plan to expand the business to reach more high schools across the province and eventually offer services to university students.",
+        description: "Advantage Academy is an online tutoring business I co-founded with the goal of providing safe and affordable assistance to high school students during the Covid-19 pandemic. Since our launch in 2020, our client base has quadrupled, and we’ve been fortunate enough to welcome a second and third wave of tutors to our team. In addition to managing our tutoring team, I’ve leveraged my technical background to build and maintain the company website as well as devise automated solutions to our billing and payroll processes. Going forward we plan to expand the business to reach more high schools across the province and eventually offer services to university students.",
         tags: [
             "JavaScript",
             "Google Apps Scripts",
@@ -33,7 +33,7 @@ let projectItems = [
     {
         title: "Raven Trade",
         imgUrl: process.env.PUBLIC_URL + '/img/raven_trade_img.png',
-        description: "Raven Trade is small web application that aims to mimic popular stock brockerage platforms such as Questrade and Wealth Simple with a theme inspired by Carleton University’s mascot, the Raven. I built this app with a good friend of mine, and it was a first experience in the world of web development for both of us. Together, we learned the fundamentals of JavaScript, templating technologies such as PUG and Jade, REST API development, Node.js, and git collaboration. While the app certainly has its shortcomings, it was a great introduction to many critical web technologies and laid the foundation for my experience as an Automation Developer the following summer.",
+        description: "Raven Trade is small web app with a theme inspired by Carleton University's mascot. Raven Trade aims to mimic popular stock brockerage platforms such as Questrade and Wealth Simple. I built this app with a good friend of mine, and it was a first experience in the world of web development for both of us. Together, we learned the fundamentals of JavaScript, templating technologies such as PUG and Jade, REST API development, Node.js, and git collaboration. While the app certainly has its shortcomings, it was a great introduction to many critical web technologies and laid the foundation for my experience as an Automation Developer the following summer.",
         tags: [
             "JavaScript",
             "Node.js",
@@ -46,7 +46,7 @@ let projectItems = [
     {
         title: "Sociallect",
         imgUrl: process.env.PUBLIC_URL + '/img/sociallect_img.png',
-        description: "Sociallect is a Software Startup I co-founded in 2020 with some close colleagues from university. Our main objective is to develop tools services that help small businesses create more effective Social Media marketing using Machine Learning. Since we started, I’ve contributed by developing our company website as well as researching options for web infrastructure and hosting. In doing so, I’ve educated myself on cloud technologies and obtained an AWS cloud practitioner certificate. We are still in very early stages and do not have a public presence yet, but if you’re interested in the project, please leave me a message and I’d be happy to discuss.",
+        description: "Sociallect is a Software Startup I co-founded in 2020 with some close colleagues from university. Our main objective is to develop tools and services that help small businesses create more effective Social Media marketing with Machine Learning. Since we started, I’ve contributed by developing our company website as well as researching options for web infrastructure and hosting. In doing so, I’ve educated myself on cloud technologies and obtained an AWS cloud practitioner certification. We are still in very early R&D stages and do not have a public presence yet, but if you’re interested in the project, please leave me a message and I’d be happy to discuss.",
         tags: [
             "JavaScript",
             "Webflow",
